6784390c647187f445f1a2c865f905fb4a3261a3
Amar Abbas <amarabbas1414@gmail.com>
2025-09-03 22:41:43 +0500

    people.js and .env updated

--- server/models/Person.js
+++ server/models/Person.js
@@ -1,17 +1,22 @@
-// server/models/Person.js
 const mongoose = require("mongoose");
 
-const personSchema = new mongoose.Schema({
-  name: { type: String, index: true, required: true },
-  description: String,
-  category: String,
-  blockchain: String,
-  device: String,
-  status: String,
-  nft: String,
-  f2p: String,
-  p2e: String,
-  p2e_score: { type: Number, default: 0 },
-}, { timestamps: true });
+const PersonSchema = new mongoose.Schema(
+  {
+    name: { type: String, index: true },
+    description: String,
+    category: String,
+    blockchain: String,
+    device: String,
+    status: String,
+    nft: String,
+    f2p: String,
+    p2e: String,
+    p2e_score: { type: Number, default: 0 },
 
-module.exports = mongoose.model("Person", personSchema);
+    // NEW: vector embedding
+    embedding: { type: [Number], default: [] },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Person", PersonSchema);
